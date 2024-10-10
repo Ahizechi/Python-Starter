ff15b64912f37f6eab1c4adcf4fd9ec8d0358427
Zechi <97638094+Ahizechi@users.noreply.github.com>
2024-10-10 10:08:00 +0100

    Delete 1 - Easy directory

--- 1 - Easy/Palindrome.py
+++
@@ -1,6 +0,0 @@
-def is_palindrome(s):
-    return s == s[::-1]
-
-# Test
-test_string = "radar"
-print(is_palindrome(test_string))
--- 1 - Easy/Temp_Converter.py
+++
@@ -1,8 +0,0 @@
-def temp_converter(temp, unit):
-    if unit == "C":
-        return (temp * 9/5) + 32  # Convert to Fahrenheit
-    elif unit == "F":
-        return (temp - 32) * 5/9  # Convert to Celsius
-
-# Test
-print(temp_converter(100, "C"))
